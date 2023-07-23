cb928fb4cb1973a10e9f79fa440e97b5057bbe3d
b-hash <brianmhayashi@gmail.com>
2023-07-23 16:12:53 -0700

    (delete) debounce since throttle exists and works similarly

--- utils/app/debounce.ts
+++
@@ -1,7 +0,0 @@
-export function debounce(callback: Function, wait = 300) {
-  let timeoutId: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(null, args), wait);
-  };
-}
